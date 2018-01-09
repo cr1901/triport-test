8fc1bc50057cc65fe5e1e4dbbf427ba3bb331850
William D. Jones <thor0505@comcast.net>
2018-01-08 23:31:50 -0500

    Parametrize further.

--- triport.v
+++ triport.v
@@ -1,25 +1,40 @@
-`define DATA_WIDTH_RANGE 7:0
+`define DATA_WIDTH_RANGE 3:0
 `define REG_WIDTH 5
 `define REG_WIDTH_RANGE (`REG_WIDTH - 1):0
 
 module triport(input clk, input rst, input [`REG_WIDTH_RANGE] rd_addr1,
-    input [`REG_WIDTH_RANGE] rd_addr2, input [`REG_WIDTH_RANGE] wr_addr,
-    output [`DATA_WIDTH_RANGE] rd_data1, output [`DATA_WIDTH_RANGE] rd_data2,
-    input [`DATA_WIDTH_RANGE] wr_data, input wen);
+`ifdef TRIPORT
+    input [`REG_WIDTH_RANGE] rd_addr2,
+`endif
+    input [`REG_WIDTH_RANGE] wr_addr, output [`DATA_WIDTH_RANGE] rd_data1,
+`ifdef TRIPORT
+    output [`DATA_WIDTH_RANGE] rd_data2,
+`endif
+    input [`DATA_WIDTH_RANGE] wr_data, input wen, input stall);
 
     wire [`REG_WIDTH_RANGE] rd_addr1;
-    wire [`REG_WIDTH_RANGE] rd_addr2;
+    `ifdef TRIPORT
+        wire [`REG_WIDTH_RANGE] rd_addr2;
+    `endif
     wire [`REG_WIDTH_RANGE] reg_rd_addr1;
-    wire [`REG_WIDTH_RANGE] reg_rd_addr2;
+    `ifdef TRIPORT
+        wire [`REG_WIDTH_RANGE] reg_rd_addr2;
+    `endif
     wire [`REG_WIDTH_RANGE] wr_addr;
 
     `ifdef WRITE_FIRST
         wire [`DATA_WIDTH_RANGE] rd_data1;
-        wire [`DATA_WIDTH_RANGE] rd_data2;
+
+        `ifdef TRIPORT
+            wire [`DATA_WIDTH_RANGE] rd_data2;
+        `endif
     `else
         `ifdef READ_FIRST
             reg [`DATA_WIDTH_RANGE] rd_data1;
-            reg [`DATA_WIDTH_RANGE] rd_data2;
+
+            `ifdef TRIPORT
+                reg [`DATA_WIDTH_RANGE] rd_data2;
+            `endif
         `endif
     `endif
 
@@ -30,7 +45,10 @@ module triport(input clk, input rst, input [`REG_WIDTH_RANGE] rd_addr1,
     always @(posedge clk) begin
         `ifdef READ_FIRST
             rd_data1 <= registers[rd_addr1];
-            rd_data2 <= registers[rd_addr2];
+
+            `ifdef TRIPORT
+                rd_data2 <= registers[rd_addr2];
+            `endif
         `endif
 
         if (wen) begin
@@ -42,14 +60,22 @@ module triport(input clk, input rst, input [`REG_WIDTH_RANGE] rd_addr1,
         // that reads are synchronous from this module alone without these
         // statements.
         `ifdef WRITE_FIRST
-            reg_rd_addr1 <= rd_addr1;
-            reg_rd_addr2 <= rd_addr2;
+            if(!stall) begin
+                reg_rd_addr1 <= rd_addr1;
+
+                `ifdef TRIPORT
+                    reg_rd_addr2 <= rd_addr2;
+                `endif
+            end
         `endif
     end
 
 `ifdef WRITE_FIRST
     assign rd_data1 = registers[reg_rd_addr1];
-    assign rd_data2 = registers[reg_rd_addr2];
+
+    `ifdef TRIPORT
+        assign rd_data2 = registers[reg_rd_addr2];
+    `endif
 `endif
 
 endmodule
